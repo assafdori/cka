01ff6155694e0f333c9cd42d3d941869f099e08a
Assaf Dori <assafdori@icloud.com>
2024-10-18 19:48:16 +0300

    fix/output context

--- terraform/context.tf
+++ terraform/context.tf
@@ -1,11 +1,12 @@
 locals {
   region = "us-east-1"
   cluster_name = module.eks.cluster_name
+  cluster_alias = "amazon-cluster"
 }
 
 output "configure_kubectl" {
   description = "Command to configure kubectl"
   value = <<-EOT
-    aws eks --region ${local.region} update-kubeconfig --name ${local.cluster_name} --alias "amazon-cluster"
+    aws eks --region ${local.region} update-kubeconfig --name ${local.cluster_name} --alias ${local.cluster_alias}
 EOT
 }
