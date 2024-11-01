d966f1da1652e839309e4a5347951821ba07810c
Assaf Dori <assafdori@icloud.com>
2024-11-01 21:54:13 +0200

    eks/adding ebs-csi driver
    should fix issues with pending pvcs

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -25,6 +25,7 @@ module "eks" {
     eks-pod-identity-agent = {}
     kube-proxy             = {}
     vpc-cni                = {}
+    aws-ebs-csi-driver     = {}
   }
 
   vpc_id                   = module.vpc.vpc_id
